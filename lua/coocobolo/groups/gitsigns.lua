095a915d10b6b6835ef4da8be5bab2de2c49cd5f
Kbgjtn <kambingjan31@gmail.com>
2024-08-12 20:15:23 +0700

    refactor(group): change color gitsigns

--- lua/coocobolo/groups/gitsigns.lua
+++ lua/coocobolo/groups/gitsigns.lua
@@ -2,9 +2,9 @@ local M = {}
 
 function M.hl(c)
 	return {
-		["GitSignsAdd"] = { fg = c.green_toska },
-		["GitSignsChange"] = { fg = c.yellow_naples },
-		["GitSignsDelete"] = { fg = c.red_orange },
+		["GitSignsAdd"] = { fg = c.green_emerald },
+		["GitSignsChange"] = { fg = c.dark_thunder },
+		["GitSignsDelete"] = { fg = c.shocking_orange },
 
 		DiffChange = { fg = c.yellow_naples, bg = c.none },
 		DiffAdd = { fg = c.green_meadow2 },
