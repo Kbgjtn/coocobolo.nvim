3007c6a05ef636426650d33b07e83e687bfc6644
Kbgjtn <kambingjan31@gmail.com>
2025-12-08 10:28:16 +0000

    feat(colors): adjust palette colors theme based

--- lua/coocobolo/colors.lua
+++ lua/coocobolo/colors.lua
@@ -1,8 +1,6 @@
 local M = {}
 
 M.base = {
-	none = "NONE",
-	dark_black = "#000000",
 	dark_erie = "#1B1B1B",
 	dark_onxy = "#101010",
 	dark_onxy2 = "#171717",
@@ -10,33 +8,12 @@ M.base = {
 	dark_rangoon = "#191919",
 	dark_jungle = "#202020",
 	dark_jungle2 = "#212121",
-	dark_thunder = "#303030",
-	dark_grey = "#353535",
-	dark_dune = "#474747",
-
-	white_dust0 = "#e2e1e0",
-	white_dust = "#9F9C9A",
-	white_dust2 = "#8E8C8A",
-	white_dust3 = "#7E7C7B",
-
-	orange_chrome = "#FFA600",
-	orange_blush = "#EEA190",
-	shocking_orange = "#E0654A",
-
-	yellow_coral = "#FAEA48",
-	yellow_candle = "#FFC436",
-	yellow_naples = "#FFD05E",
-	yellow_naples_90 = "#ffe099",
-
 	grey_corporate = "#5C5C5C",
 	grey_simple = "#676765",
-	grey_davy = "#555555",
-	dim_grey = "#606060",
+	stone_cold = "#555555",
+	kettleman = "#606060",
 	grey_gravel = "#484848",
-	grey_smokey = "#707070",
 	grey_slate = "#BAB8B4",
-
-	green_shadow = "#898B55",
 	green_meadow = "#71AC1E",
 	green_meadow2 = "#95C92C",
 	green_dull = "#D9D930",
@@ -46,45 +23,51 @@ M.base = {
 	green_limerick = "#A5BE00",
 	green_neon = "#9ACD32",
 	green_coral = "#82CD47",
-
 	darkolivegreen = "#5a5e48",
-
-	yellowgreen = "#95C92C",
-
-	grape = "#B9C0EA",
-
 	red_orange = "#F26E5A",
 	red_coral = "#FF4545",
-
+	grape = "#B9C0EA",
 	grape_elsie = "#D79FC7",
 	grape_elsie2 = "#564050",
-
 	blue_light_teal = "#B1CCC5",
-	blue_deepsky = "#00BFFF",
+	blue_heavy_teal = "#7f804f",
 	blue_deepsky_30 = "#2084af",
-
 	blue_lowkey = "#B5CBCC",
 	blue_lowkey2 = "#BECDDB",
-
+	yellow_coral = "#FAEA48",
+	yellow_candle = "#FFC436",
+	yellow_naples = "#FFD05E",
+	yellow_naples_90 = "#FFE099",
+	yellowgreen = "#95C92C",
 	yellow_sunglow = "#FFC436",
-	goldenrod = "#FFC436",
 	yellow_sunglow_dim = "#FFD05E",
-
+	goldenrod = "#FFC436",
 	orange_beer = "#FB8B24",
 	orange_chinese = "#FFB000",
 	orange_soft = "#F3B664",
+	orange_chrome = "#FFA600",
+	orange_blush = "#EEA190",
+	shocking_orange = "#E0654A",
 
-	neutral_10 = "#ffffff",
-	neutral_15 = "#8c898c",
-	neutral_20 = "#555554",
-	neutral_30 = "#444443",
-	neutral_40 = "#343433",
-	neutral_50 = "#252524",
-	neutral_60 = "#161616",
-
-	crimson_95 = "#d22737",
-	crimson_40 = "#e3635f",
-	crimson_25 = "#882326",
+	none = "NONE",
+	black = "#000000",
+	pot_black = "#161616",
+	crimson = "#E3635F",
+	dark_slate_gray = "#444443",
+	witch_brew = "#8A8635",
+	amber = "#FFBF00",
+	off_black = "#303030",
+	dark_grey = "#353535",
+	black_tie = "#474747",
+	smokey_wings = "#B1ADAB",
+	equilibrium = "#a5a09e",
+	pedigrey = "#8E8C8A",
+	green_shadow = "#7A7B4C",
+	charcoal_smudge = "#605f5e",
+	blue_deepsky = "#00BFFF",
+	silver = "#BAB8B4",
+	dugong = "#707070",
+	dark_gray = "#AEABA7",
 }
 
 local merge = function(a, b)
@@ -99,20 +82,35 @@ local merge = function(a, b)
 end
 
 M.dark_palette = {
-	bg = M.base.dark_black,
-	fg = M.base.neutral_30,
-	primary = M.base.neutral_60,
+	bg = M.base.black,
+	fg = M.base.dark_slate_gray,
+	primary = M.base.pot_black,
 	primary_2 = M.base.dark_grey,
-	primary_3 = M.base.white_dust3,
-	primary_4 = M.base.white_dust2,
-	primary_5 = M.base.white_dust,
-	primary_6 = M.base.white_dust0,
+	primary_3 = M.base.charcoal_smudge,
+	primary_4 = M.base.pedigrey,
+	primary_5 = M.base.equilibrium,
+	primary_6 = M.base.smokey_wings,
+	red = M.base.crimson,
+	green = M.base.green_shadow,
+	blue = M.base.blue_deepsky,
+	yellow = M.base.yellow_candle,
+	accent = M.base.green_shadow,
 }
 
 M.colorful_palette = {
-	bg = M.base.white_dust3,
-	fg = M.base.dark_black,
-	primary = M.base.dark_grey,
+	bg = M.base.silver,
+	fg = M.base.black,
+	primary = M.base.dark_gray,
+	primary_6 = M.base.black_tie,
+	primary_5 = M.base.stone_cold,
+	primary_4 = M.base.kettleman,
+	primary_3 = M.base.off_black,
+	primary_2 = M.base.dugong,
+	red = M.base.crimson,
+	green = M.base.witch_brew,
+	blue = M.base.blue_deepsky,
+	yellow = M.base.amber,
+	accent = M.base.witch_brew,
 }
 
 M.dark_palette = merge(M.base, M.dark_palette)
