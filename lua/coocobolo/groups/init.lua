476c42adf400bf39f08fbdbb9182ea222ba4ebf7
Kbgjtn <kambingjan31@gmail.com>
2025-12-08 10:28:41 +0000

    fix: implicit using base one file

--- lua/coocobolo/groups/init.lua
+++ lua/coocobolo/groups/init.lua
@@ -1,13 +1,3 @@
 return {
-	require("coocobolo.groups.base_2"),
-	-- require("coocobolo.groups.go"),
-	-- require("coocobolo.groups.lazy"),
-	-- require("coocobolo.groups.yaml"),
-	-- require("coocobolo.groups.dap-ui"),
-	-- require("coocobolo.groups.neotest"),
-	-- require("coocobolo.groups.coverage"),
-	-- require("coocobolo.groups.gitsigns"),
-	-- require("coocobolo.groups.telescope"),
-	-- require("coocobolo.groups.fzf-lua"),
-	-- require("coocobolo.groups.fugitive"),
+	require("coocobolo.groups.base"),
 }
